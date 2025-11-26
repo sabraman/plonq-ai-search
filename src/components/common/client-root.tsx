17574f69f814570b96843d1f6aeb89a0125bb194
sabraman <sabraman@yandex.ru>
2025-11-26 09:29:40 +0300

    Initial commit

---
+++ src/components/common/client-root.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import dynamic from "next/dynamic";
+import type { ReactNode } from "react";
+import Loader from "./loader";
+import { ThemeProvider } from "./theme-provider";
+
+// Dynamically import the Root component with ssr: false
+const DynamicRoot = dynamic(() => import("./root"), {
+  ssr: false,
+  loading: () => <Loader />,
+});
+
+// Create Convex client
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL;
+if (!convexUrl) {
+  throw new Error("NEXT_PUBLIC_CONVEX_URL is not set");
+}
+const convex = new ConvexReactClient(convexUrl);
+
+// Client wrapper component that can be imported in layout.tsx
+export function ClientRoot({
+  children,
+  debug = false,
+}: {
+  children: ReactNode;
+  debug?: boolean;
+}) {
+  return (
+    <ConvexProvider client={convex}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <DynamicRoot debug={debug}>{children}</DynamicRoot>
+      </ThemeProvider>
+    </ConvexProvider>
+  );
+} 
